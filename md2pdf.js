d0dbe083e14ccc64a031f95b5828529d34d7fecc
futabooo <takahiro.futagawa@gmail.com>
2020-05-16 01:38:57 +0900

    add md2pdf script

---
+++ md2pdf.js
@@ -0,0 +1,12 @@
+var markdownpdf = require("markdown-pdf")
+
+// markdownpdf().from("./jp/README.md").to("./jp/README.pdf", function () {
+// console.log("Done")
+// })
+
+var mdDocs = ["./jp/README.md", "./en/README.md"]
+    , pdfDocs = mdDocs.map(function (d) { return d.replace(".md", ".pdf") })
+
+markdownpdf().from(mdDocs).to(pdfDocs, function () {
+    pdfDocs.forEach(function (d) { console.log("Created", d) })
+})
