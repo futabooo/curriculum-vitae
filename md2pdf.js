2223a9b60ed600fd453d00394624466ffec95320
futabooo <takahiro.futagawa@gmail.com>
2023-02-11 23:30:05 +0900

    add style css

--- md2pdf.js
+++ md2pdf.js
@@ -1,12 +1,18 @@
-var markdownpdf = require("markdown-pdf")
+var markdownpdf = require("markdown-pdf");
 
 // markdownpdf().from("./jp/README.md").to("./jp/README.pdf", function () {
 // console.log("Done")
 // })
 
-var mdDocs = ["./jp/README.md", "./en/README.md"]
-    , pdfDocs = mdDocs.map(function (d) { return d.replace(".md", ".pdf") })
+var mdDocs = ["./jp/README.md", "./en/README.md"],
+  pdfDocs = mdDocs.map(function (d) {
+    return d.replace(".md", ".pdf");
+  });
 
-markdownpdf().from(mdDocs).to(pdfDocs, function () {
-    pdfDocs.forEach(function (d) { console.log("Created", d) })
-})
+markdownpdf({ cssPath: "./pdfstyle.css" })
+  .from(mdDocs)
+  .to(pdfDocs, function () {
+    pdfDocs.forEach(function (d) {
+      console.log("Created", d);
+    });
+  });
